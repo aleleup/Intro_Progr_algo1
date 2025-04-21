35d0dbe7503f790239d8a821bd3b6f94ba12f4c0
Alejandro Leupold de Souza <aleupold@dappsfactory.io>
2025-04-21 17:40:14 -0300

    a ver si los hice bien en clase

--- Guia5/Ej3.hs
+++ Guia5/Ej3.hs
@@ -36,4 +36,55 @@ sumarN n (x:xs) = (n + x) : sumarN n xs
 sumarElPrimero :: [Integer] -> [Integer]
 
 sumarElPrimero (x:xs) | xs == [] = []
-                      | otherwise =  sumarN x (xs)
+                      | otherwise =  sumarN x (x:xs)
+--
+
+sumarElUltimo (x:xs) | xs == [] = []
+                     | otherwise = sumarN (ultimo (x:xs)) (x:xs)
+--del ej1
+ultimo:: Eq t => [t] -> t
+ultimo (x:xs) | xs == [] = x
+              | otherwise = ultimo xs
+--
+
+
+pares :: [Integer] -> [Integer]
+pares [] = []
+pares (x:xs) | mod x 2 == 0 = x: pares xs       
+             | otherwise = pares xs
+
+--
+
+
+
+multiplosDeN :: Integer -> [Integer] -> [Integer]
+
+multiplosDeN _ [] = []
+multiplosDeN n (x:xs) | mod x n == 0 = x : multiplosDeN n xs
+                      | otherwise = multiplosDeN n xs
+
+
+--
+
+
+ordenar :: [Integer] -> [Integer]
+
+ordenar (x:xs) | xs == [] = x : []
+                | otherwise = minimo (x:xs) : ordenar  (quitar (minimo (x:xs)) (x:xs) (False))
+
+
+
+quitar  :: Eq t => t -> [t] ->Bool -> [t]
+quitar t (x:xs) logic | xs == [] && x /= t = x : []
+                | xs == [] && x == t = []
+                | x == t && not logic = quitar t xs True
+                | otherwise = x : quitar t xs logic
+
+minimo :: [Integer] -> Integer
+minimo (x:y:xs)    | xs == [] = menor x y
+                   | x <= y = menor x (minimo (y:xs))
+                   | otherwise = menor y (minimo (y:xs))
+
+menor :: Integer -> Integer -> Integer
+menor x y | x <= y = x
+        | otherwise = y
