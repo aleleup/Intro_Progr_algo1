4e99d2fd2429c194f3855506a30aab740bdc6c18
Tomás Garaglia <garaglia.perezfernandez@gmail.com>
2025-05-12 20:59:48 -0300

    Mejorando

--- Guia6/Ej4.py
+++ Guia6/Ej4.py
@@ -1,20 +1,25 @@
 def peso_pino(metros:int) -> int:
-    if metros <= 3: return 300 * metros
+    x:int = 0
+    if metros <= 3: x = 300 * metros
     else: 
         metros-=3
-        return 900 + 200*metros
-    
+        x = 900 + 200*metros
+    return x
 
 def es_peso_util(peso_pino:int) -> bool:
+    x:bool = False
     peso_esperado:int = 900
     if peso_pino >= peso_esperado: return True
-    return False
+    
+    return x
 
 
 def sirve_pino(altura:int) -> bool:
+    x:bool = False
     peso_esperado:int = 900
     if peso_pino(altura) >= peso_esperado: return True
-    return False
+    
+    return x
 
 
 
